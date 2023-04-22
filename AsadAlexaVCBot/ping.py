6f0def01c328d4a1961ce98765164900ec94585d
kontolkeboo13 <130371222+kontolkeboo13@users.noreply.github.com>
2023-04-22 09:46:22 +0700

    Update ping.py

--- AsadAlexaVCBot/ping.py
+++ AsadAlexaVCBot/ping.py
@@ -41,7 +41,7 @@ async def ping(client, m: Message):
     delta_ping = time() - start
     uptime_sec = (current_time - START_TIME).total_seconds()
     uptime = await _human_time_duration(int(uptime_sec))
-    await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Uptime ⏳** - `{uptime}`")
+    await m_reply.edit(f"`{delta_ping * pong 1000:.3f} ms` \n**Uptime ** - `{uptime}`")
 
 
 @Client.on_message(contact_filter & filters.command(["restart"], prefixes=f"{HNDLR}"))
